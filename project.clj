3fe16900349e3181eb88eec498e18cbd74d66f7e
Kim Kinnear <kkinnear@cisco.com>
2017-04-10 20:17:39 -0400

    Update project.clj to better reflect multipe build requirements.

--- project.clj
+++ project.clj
@@ -8,11 +8,13 @@
             :year 2015}
   :plugins [[lein-expectations "0.0.8"] [lein-zprint "0.2.0"]]
   :profiles {:dev {:dependencies [[expectations "2.0.16"]
-                                  [com.taoensso/tufte "1.1.1"]]},
+                                  [com.taoensso/tufte "1.1.1"]
+				  [clojure-future-spec "1.9.0-alpha15"]]},
              :uberjar {:aot [zprint.core zprint.main],
                        :main zprint.main,
+		       :dependencies [[clojure-future-spec "1.9.0-alpha15"]]
 		       :omit-source true
-                       :uberjar-name "zprint-filter-0.3.0"}}
+                       :uberjar-name "zprint-filter-0.3.1"}}
   ; Clojure 1.8 you can exclude all sources
   :uberjar-exclusions [#"\.(clj|java|cljs)"]
   ; Clojure 1.9 requires the .clj files

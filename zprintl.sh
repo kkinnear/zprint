4bbfc0579f8088cad6d7e4e3c85c93cbdaed5dc9
Kim Kinnear <kkinnear@cisco.com>
2018-11-06 20:43:12 -0500

    Fix zprintl.sh so that we get the right graalvm for Linux.

--- zprintl.sh
+++ zprintl.sh
@@ -1,7 +1,10 @@
 # First argument -- location of graalvm
 # Second argument -- version of zprint-filter
 cd /app
-export PATH=$PATH:$1/Contents/Home/bin
-export JAVA_HOME=$1/Contents/Home
+export PATH=$1/bin:$PATH
+export JAVA_HOME=$1
+echo "$PATH"
+pwd
+whereis native-image
 native-image --version
 native-image --no-server -J-Xmx4G -J-Xms2G -jar target/zprint-filter-$2 -H:Name="zprintl-$2" -H:+ReportUnsupportedElementsAtRuntime

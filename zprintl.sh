1ab5f81e6df06023ab6eea5e2d86114adc5d8d06
mynomoto <mynomoto@gmail.com>
2018-11-30 16:01:39 -0200

    Use an UTF-8 locale to build the linux native image

--- zprintl.sh
+++ zprintl.sh
@@ -7,4 +7,4 @@ echo "$PATH"
 pwd
 whereis native-image
 native-image --version
-native-image --no-server -J-Xmx4G -J-Xms2G -jar target/zprint-filter-$2 -H:Name="zprintl-$2" -H:+ReportUnsupportedElementsAtRuntime
+LC_ALL=C.UTF-8 native-image --no-server -J-Xmx4G -J-Xms2G -jar target/zprint-filter-$2 -H:Name="zprintl-$2" -H:+ReportUnsupportedElementsAtRuntime

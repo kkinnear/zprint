d95488d7d0265bb485c40a686a8872e24e67a7bc
Kim Kinnear <kim@kinnear.com>
2021-01-18 13:23:26 -0500

    Update to new way to build zprintl -- using modern docker images.

--- zprintl.sh
+++ zprintl.sh
@@ -1,16 +1,6 @@
-# First argument -- location of graalvm
-# Second argument -- file name of native image in graalvml directory
-# Third argument -- version of zprint-filter
-cd /app
-export PATH=$1/bin:$PATH
-export JAVA_HOME=$1
-echo "$PATH"
-pwd
-apt-get update
-apt-get -y install libstdc++
-gu install -L $2
-whereis native-image
+# first argument -- version of zprint-filter
+gu install native-image
 native-image --version
-LC_ALL=C.UTF-8 native-image --no-server -J-Xmx7G -J-Xms4G -jar target/zprint-filter-$3 -H:Name="zprintl-$3" -H:EnableURLProtocols=https,http -H:+ReportExceptionStackTraces --report-unsupported-elements-at-runtime --initialize-at-build-time --no-fallback
-
-
+cd /app
+pwd
+LC_ALL=C.UTF-8 native-image --no-server -J-Xmx7G -J-Xms4G -jar target/zprint-filter-$1 -H:Name="zprintl-$1" -H:EnableURLProtocols=https,http -H:+ReportExceptionStackTraces --report-unsupported-elements-at-runtime --initialize-at-build-time --no-fallback
